d0c270d444e77e5c36ccc35cf23ce1effa9b0da7
fxrel <farelhidayat123@gmail.com>
2021-03-30 18:13:12 +0700

    Insert 20 data in BarangSeeder

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -14,28 +14,33 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        Barang::create([
-            'kode_barang'       => 'PRD001',
-            'nama_barang'       => 'Indomie',
-            'kategori_barang'   => 'Makanan',
-            'harga'             => 3000,
-            'qty'               => 100,
-        ]);
+        $kode_barang =['PRD001', 'PRD002', 'PRD003', 'PRD004', 'PRD005',
+                        'PRD006', 'PRD007', 'PRD008', 'PRD009', 'PRD010',
+                        'PRD011', 'PRD012', 'PRD013', 'PRD014', 'PRD015',
+                        'PRD016', 'PRD017', 'PRD018', 'PRD019', 'PRD020'];
+        $nama_barang =['Indomie', 'Pocari Sweat', 'Silverqueen', 'Potabee', 'Aqua',
+                        'Mie Sedaap', 'Sari Roti', 'Lemonilo', 'Sarimi', 'Paroti',
+                        'Teh Pucuk', 'UC1000', 'Cimori', 'Conetto', 'Magnum',
+                            'Citato', 'Cheetos', 'Pringles', 'Lays', 'Soyjoy'];
+        $kategori_barang =['Makanan','Minuman', 'Snack', 'Snack', 'Minuman',
+                            'Makanan', 'Makanan', 'Makanan', 'Makanan', 'Makanan',
+                            'Minuman', 'Minuman', 'Minuman', 'Es Krim', 'Es Krim', 
+                            'Snack', 'Snack', 'Snack', 'Snack', 'Snack'];
+        $harga =['2500', '6000', '25000', '7500', '2500',
+                '2500', '4500', '4500', '3000', '4000',
+                '3500', '7000', '10000', '15000', '15000',
+                '7000', '9000', '12000', '1100', '7500'];
+        $qty =['25', '10', '11', '5', '10', '7', '8', '9', '10', '12',
+                '13', '15', '17', '19', '20', '23', '25', '35', '18', '20'];
 
-        Barang::create([
-            'kode_barang'       => 'PRD002',
-            'nama_barang'       => 'Pocari Sweat',
-            'kategori_barang'   => 'Minuman',
-            'harga'             => 6000,
-            'qty'               => 50,
-        ]);
-
-        Barang::create([
-            'kode_barang'       => 'PRD003',
-            'nama_barang'       => 'Silverqueen',
-            'kategori_barang'   => 'Snack',
-            'harga'             => 12500,
-            'qty'               => 45,
-        ]);
+        for($i=0; $i < 20; $i++){
+            Barang::insert([
+                'kode_barang' => $kode_barang[$i],
+                'nama_barang' => $nama_barang[$i],
+                'kategori_barang' => $kategori_barang[$i],
+                'harga' => $harga[$i],
+                'qty' => $qty[$i]
+            ]);
+        }
     }
 }
